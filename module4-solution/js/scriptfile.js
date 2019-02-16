2e9433fa7be36bbfd7f04c1b993c863c9093d88f
Muhannad-A9 <Muhannad-a9@hotmail.com>
2019-02-16 20:12:34 +0100

    Delete scriptfile.js

--- module4-solution/js/scriptfile.js
+++
@@ -1,9 +0,0 @@
-var arraynames=["John","Laila","Muhannad","josef","mai","Fattoh"]
-
-for (var i = 0; i < arraynames.length; i++) {
-	var string= arraynames[i].slice(0,1);
-	if(string=="j" || string=="J")
-	console.log("Goodbye  "+arraynames[i]) 
-else
-	console.log("Hello   "+arraynames[i]) 
-}
